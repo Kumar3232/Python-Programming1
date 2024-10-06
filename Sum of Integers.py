58e341a0ecdcc5829ac49297c7229bcff076545b
Kumar3232 <vishalkumar@metropolia.fi>
2024-10-06 18:58:44 +0300

    Add files via upload

--- Sum of Integers.py
+++ Sum of Integers.py
@@ -1,1 +1,15 @@
+def sum_of_list(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+
+def main():
+    my_list = [23, 32, 12, 6, 54]
+
+    result = sum_of_list(my_list)
+
+    print(f"The sum of the list is: {result}")
+
 main()
